683633471add5e8b5b50a9d49a2873525752c612
eminmengi <eminmengi@gmail.com>
2021-07-28 11:20:02 +0300

    sanctum kuruldu

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('get_tweets',[TweetsController::class,'get_tweets'])->name('get_tweets');
+Route::middleware('auth:sanctum')->get('get_tweets',[TweetsController::class,'get_tweets'])->name('get_tweets');
